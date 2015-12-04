3f71c5b2439b80893b41d2cb6d1b07c43330c067
İnstitution developer <institution-developer@outlook.com>
2015-12-04 17:19:16 +0200

    Record RTC

---
+++ RecordRTC/RecordRTC/Controllers/RecordRTCController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RecordRTC.Controllers
+{
+    using System.IO;
+    public class RecordRTCController : Controller
+    {
+        // GET: RecordRTC
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult PostRecordedAudioVideo()
+        {
+            foreach (string upload in Request.Files)
+            {
+                var path = AppDomain.CurrentDomain.BaseDirectory + "uploads/";
+                var file = Request.Files[upload];
+                if (file == null) continue;
+
+                file.SaveAs(Path.Combine(path, Request.Form[0]));
+            }
+            return Json(Request.Form[0]);
+        }
+
+        // ---/RecordRTC/DeleteFile
+        [HttpPost]
+        public ActionResult DeleteFile()
+        {
+            var fileUrl = AppDomain.CurrentDomain.BaseDirectory + "uploads/" + Request.Form["delete-file"];
+            new FileInfo(fileUrl + ".wav").Delete();
+            new FileInfo(fileUrl + ".webm").Delete();
+            return Json(true);
+        }
+    }
+}
---
+++ RecordRTC/RecordRTC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RecordRTC.MvcApplication" Language="C#" %>
